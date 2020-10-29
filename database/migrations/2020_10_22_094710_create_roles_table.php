e9eaf9d28fc22d03bd8a2b39d79ba558b14739ec
thienhuong98 <69622441+thienhuong98@users.noreply.github.com>
2020-10-29 12:30:37 +0700

    fix migrate

--- database/migrations/2020_10_22_094710_create_roles_table.php
+++ database/migrations/2020_10_22_094710_create_roles_table.php
@@ -16,6 +16,7 @@ class CreateRolesTable extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->integer('roles')->nullable();
+            $table->string('role_name')->nullable();
             $table->timestamps();
         });
     }
